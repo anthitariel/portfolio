6ab79fdd8b824e3e8bfb5b5105b83ec481d67e29
Anfisa <anfiya17@gmail.com>
2021-12-08 20:01:52 +0200

    js+animation (1)
    js+animation (1)

--- js/main.js
+++ js/main.js
@@ -2,5 +2,4 @@ let btn = document.getElementById('btn');
 btn.onclick = function() {
     let text = document.querySelector('p.intro');
     text.classList.add('red')
-    alert('Привет, мой дружочек Женечка!')
 }
