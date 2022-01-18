7c2ee030d3fa5e0443054aac26a20af7d1ebac43
Anfisa <anfiya17@gmail.com>
2022-01-18 22:30:57 +0200

    Stack+

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
+const $ = {}
+window.$ = $
+
+/* Landing - Button */
+
 let btn = document.getElementById('btn');
 btn.onclick = function() {
     let text = document.querySelector('p.intro');
     text.classList.add('red')
+}
+
+/* Modal */
+
+$.modal = function() {
+    
 }
