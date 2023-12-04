b8684fff17a303468703bde535fb322d8322b08d
Anfisa <anfiya17@gmail.com>
2023-12-04 11:09:49 +0200

    minor changes

--- js/main.js
+++ js/main.js
@@ -1,20 +1,22 @@
-// the certificate popup
-const wayUpCertificate = $("#wayUpCertificate");
-const certificatePopup = $("#certificatePopup");
-const overlay = $(".overlay");
-const closePopupBtn = $("#closePopupBtn");
+$(document).ready(function () {
+  // the certificate popup
+  const wayUpCertificate = $("#wayUpCertificate");
+  const certificatePopup = $("#certificatePopup");
+  const overlay = $(".overlay");
+  const closePopupBtn = $("#closePopupBtn");
 
-wayUpCertificate.on("click", function () {
-  certificatePopup.show();
-  overlay.show();
-});
+  wayUpCertificate.on("click", function () {
+    certificatePopup.show();
+    overlay.show();
+  });
 
-closePopupBtn.on("click", function () {
-  certificatePopup.hide();
-  overlay.hide();
-});
+  closePopupBtn.on("click", function () {
+    certificatePopup.hide();
+    overlay.hide();
+  });
 
-overlay.on("click", function () {
-  certificatePopup.hide();
-  overlay.hide();
+  overlay.on("click", function () {
+    certificatePopup.hide();
+    overlay.hide();
+  });
 });
