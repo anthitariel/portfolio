a154c5f5b7e29e4d4df55f465afc723b4797880f
Anfisa <anfiya17@gmail.com>
2023-12-06 14:00:55 +0200

    added floating icon

--- js/main.js
+++ js/main.js
@@ -12,6 +12,30 @@ $(document).ready(function () {
     $("#introOverlay").hide();
   }
 
+  // floating icon
+  setTimeout(() => {
+    changeIconPosition();
+    setInterval(changeIconPosition, 10000);
+  }, 5000);
+
+  $(document).on("click", function () {
+    changeIconPosition();
+  });
+
+  function changeIconPosition() {
+    const icon = $("#floatingIcon");
+    const newPosition = getRandomPosition();
+    icon.stop().animate({ top: newPosition.top, left: newPosition.left });
+  }
+
+  function getRandomPosition() {
+    const windowHeight = $(window).height() - 50;
+    const windowWidth = $(window).width() - 50;
+    const top = Math.random() * windowHeight;
+    const left = Math.random() * windowWidth;
+    return { top: top, left: left };
+  }
+
   // the certificate popup
   const wayUpCertificate = $("#wayUpCertificate");
   const certificatePopup = $("#certificatePopup");
