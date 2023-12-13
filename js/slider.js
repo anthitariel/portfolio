889096416a7bedac74857205a5916efb91f9e74d
Anfisa <anfiya17@gmail.com>
2023-12-13 17:40:47 +0200

    slider -> 2 slides

--- js/slider.js
+++ js/slider.js
@@ -1,19 +1,12 @@
 $("#slick").slick({
   infinite: true,
   speed: 300,
-  slidesToShow: 3,
-  slidesToScroll: 3,
+  slidesToShow: 2,
+  slidesToScroll: 2,
   adaptiveHeight: true,
   prevArrow: "<span class='slick-prev'>&lt;</span>",
   nextArrow: "<span class='slick-next'>&gt;</span>",
   responsive: [
-    {
-      breakpoint: 992,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2,
-      },
-    },
     {
       breakpoint: 576,
       settings: {
